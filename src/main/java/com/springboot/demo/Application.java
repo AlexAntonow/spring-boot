a86b4a71861aad2603df993525c22e27e3cb9081
Alex Antonow <alex.antonow@outlook.com>
2023-04-19 05:45:59 -0700

    logging, OpenAPI, HealthCheck

--- src/main/java/com/springboot/demo/Application.java
+++ src/main/java/com/springboot/demo/Application.java
@@ -1,9 +1,13 @@
 package com.springboot.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Spring Boot project", version = "1.0",
+description = "Learning project"))
 public class Application {
 
 	public static void main(String[] args) {
